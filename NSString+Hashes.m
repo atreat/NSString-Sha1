b347f6b08cbd5fdc83bb2a7195eb5b26bd310bb4
Klaus-Peter Dudas <k.dudas@liv.ac.uk>
2012-03-29 10:35:38 +0100

    Updated to NSString (Hashes) to include MD5, SHA1, SHA256 and SHA512

---
+++ NSString+Hashes.m
@@ -0,0 +1,83 @@
+//
+//  NSString+Hashes.m
+//  LIFTUPP
+//
+//  Created by Klaus-Peter Dudas on 26/07/2011.
+//  Copyright: Do whatever you want with this, i.e. Public Domain
+//
+
+#import <CommonCrypto/CommonDigest.h>
+
+#import "NSString+Hashes.h"
+
+@implementation NSString (Hashes)
+
+- (NSString *)md5
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_MD5_DIGEST_LENGTH];
+
+    CC_MD5(data.bytes, data.length, digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for (int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+    {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+}
+
+- (NSString *)sha1
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for (int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+    {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+}
+
+- (NSString *)sha256
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_SHA256_DIGEST_LENGTH];
+    
+    CC_SHA256(data.bytes, data.length, digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA256_DIGEST_LENGTH * 2];
+    
+    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++)
+    {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+}
+
+- (NSString *)sha512
+{
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    uint8_t digest[CC_SHA512_DIGEST_LENGTH];
+    
+    CC_SHA512(data.bytes, data.length, digest);
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA512_DIGEST_LENGTH * 2];
+    
+    for (int i = 0; i < CC_SHA512_DIGEST_LENGTH; i++)
+    {
+        [output appendFormat:@"%02x", digest[i]];
+    }
+    
+    return output;
+}
+
+@end
